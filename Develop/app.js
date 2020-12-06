d9fbfd71078135058d2f2a34483abeb7b066bc16
KcRaymond <kaseyleigh1978@gmail.com>
2020-12-06 15:16:25 -0700

    Added prompts using Inquirer to collect Manager input

--- Develop/app.js
+++ Develop/app.js
@@ -11,5 +11,54 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
 
-// Write code to use inquirer to gather information about the development team members,
+//create variable to store input in array
+let employees = [];
+
+//Creates a Manager profile from user input
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is the name of your Manager?",
+      name: "managerName",
+    },
+    {
+      type: "input",
+      message: "What is the ID number for your Manager?",
+      name: "managerId",
+    },
+    {
+      type: "input",
+      message: "What is the email for your Manager?",
+      name: "managerEmail",
+      default: () => {},
+          validate: function (email) {
+  
+              valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+  
+              if (valid) {
+                  return true;
+              } else {
+                  console.log("  > Please enter a valid email...")
+                  return false;}
+    },
+    },
+    {
+      type: "input",
+      message: "What is the office number for your Manager?",
+      name: "managerOfficeNumber",
+      default: "number",
+    },
+  ])
+  .then((answers) => {
+    const manager = new Manager(
+      answers.managerName,
+      answers.managerId,
+      answers.managerEmail,
+      answers.managerOfficeNumber
+    );
+    employees.push(manager);
+    createTeam();
+  });
+
 
